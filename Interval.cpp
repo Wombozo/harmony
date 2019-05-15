7dc484f78d49de8def87c89205fd3136900a8b73
guillaume <guillaume.lavigne@free.fr>
2019-05-15 20:24:28 +0200

    Add semitones implementation

--- Interval.cpp
+++ Interval.cpp
@@ -62,93 +62,92 @@ Interval::Interval(std::string str)
     else if (!str.compare("diminished third"))
     {
         type = 3;
-        semitones = 1;
+        semitones = 2;
     }
     else if (!str.compare("minor third"))
     {
         type = 3;
-        semitones = 1;
+        semitones = 3;
     }
     else if (!str.compare("major third"))
     {
         type = 3;
-        semitones = 1;
+        semitones = 4;
     }
     else if (!str.compare("augmented third"))
     {
         type = 3;
-        semitones = 1;
+        semitones = 5;
     }
     else if (!str.compare("diminished fourth"))
     {
         type = 4;
-        semitones = 1;
+        semitones = 4;
     }
     else if (!str.compare("perfect fourth"))
     {
         type = 4;
-        semitones = 1;
+        semitones = 5;
     }
     else if (!str.compare("augmented fourth"))
     {
         type = 4;
-        semitones = 1;
+        semitones = 6;
     }
     else if (!str.compare("diminished fifth"))
     {
         type = 5;
-        semitones = 1;
+        semitones = 6;
     }
     else if (!str.compare("perfect fifth"))
     {
         type = 5;
-        semitones = 1;
+        semitones = 7;
     }
     else if (!str.compare("augmented fifth"))
     {
         type = 5;
-        semitones = 1;
+        semitones = 8;
     }
     else if (!str.compare("diminished sixth"))
     {
         type = 6;
-        semitones = 1;
+        semitones = 7;
     }
     else if (!str.compare("minor sixth"))
     {
         type = 6;
-        semitones = 1;
+        semitones = 8;
     }
     else if (!str.compare("major sixth"))
     {
         type = 6;
-        semitones = 1;
+        semitones = 9;
     }
     else if (!str.compare("augmented sixth"))
     {
         type = 6;
-        semitones = 1;
+        semitones = 10;
     }
     else if (!str.compare("diminished seventh"))
     {
         type = 7;
-        semitones = 1;
+        semitones = 9;
     }
     else if (!str.compare("minor seventh"))
     {
         type = 7;
-        semitones = 1;
+        semitones = 10;
     }
     else if (!str.compare("major seventh"))
     {
         type = 7;
-        semitones = 1;
+        semitones = 11;
     }
     else
     {
         throw E_INTERVAL;
     }
-    
 }
 
 Interval::~Interval()
