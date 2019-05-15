8e64bcaf9071b70bef68ac9b3f27a1bd3bda8dae
guillaume <guillaume.lavigne@free.fr>
2019-05-15 19:42:10 +0200

    Adding notes as extern

--- Note.h
+++ Note.h
@@ -3,12 +3,20 @@
 
 #include <iostream>
 #include <string>
+#include "errors.h"
 
 class Note
 {
 public:
     virtual std::string display(void) = 0;
     virtual int semiToneCount(void) = 0;
+    static void initialize(void);
 };
+
+extern Note *A, *B, *C, *D, *E, *F, *G;
+extern Note *Ab, *Bb, *Cb, *Db, *Eb, *Fb, *Gb;
+extern Note *As, *Bs, *Cs, *Ds, *Es, *Fs, *Gs;
+extern Note *Abb, *Bbb, *Cbb, *Dbb, *Ebb, *Fbb, *Gbb;
+extern Note *Ass, *Bss, *Css, *Dss, *Ess, *Fss, *Gss;
 
 #endif
