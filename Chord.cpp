3ddff5214963f79d794ead9ed4d7439acbb24833
Guillaume Lavigne <guillaume.lavigne@smile.fr>
2019-05-16 14:41:33 +0200

    Moving on (save)

--- Chord.cpp
+++ Chord.cpp
@@ -1,30 +1,81 @@
 #include "Chord.h"
 #include "NaturalNote.h"
+#include "DblFlatNote.h"
+#include "DblSharpNote.h"
+#include "SharpNote.h"
+#include "FlatNote.h"
 
 //#define I_EQUALS(i, str) (!(intervals.at(i).getName().compare(str)))
 
 void Chord::setNotes()
 {
+    std::cout << root->display() << std::endl;
     notes.push_back(root);
-    int type = intervals.at(0).getType();
-    NaturalNote *tmp = root->getNatural();
-    std::cout << root->getNatural()->display() << std::endl;
-    for (int i = 0; i < type - 1; i++)
+    for (int k=0; k < intervals.size(); k++)
     {
-        tmp = tmp->following;
+        int type = intervals.at(k).getType();
+        int rst = intervals.at(k).getSemiTones();
+        Note *tmp;
+        SharpNote *stmp;
+        FlatNote *ftmp;
+        DblSharpNote *sstmp;
+        DblFlatNote *fftmp;
+        NaturalNote *ntmp = root->getNatural();
+        // Get the natural note
+        for (int i = 0; i < type - 1; i++)
+        {
+            ntmp = ntmp->following;
+        }
+      //  std::cout << "nat = "<<ntmp->getFName()<< " ";
+      //  std::cout << "ntmp "<< ntmp->semiToneCount()<<" rst + rstc "<<rst<<" + "<<root->semiToneCount()<<std::endl;
+        // Test for altered notes
+        if (ntmp->semiToneCount() == rst + root->semiToneCount())
+        {
+            // Natural note;
+            tmp = (Note *) ntmp;
+        }
+        else if(ntmp->semiToneCount() == rst + root->semiToneCount() - 1)
+        {
+            // Sharp note
+            stmp = ntmp->sn;    
+            tmp = (Note*) stmp;
+        }
+        else if((ntmp->semiToneCount() == rst + root->semiToneCount() + 1))
+        {
+            // Flat note
+            ftmp = ntmp->fn;    
+            tmp = (Note*) ftmp;
+        }
+        else if(ntmp->semiToneCount() == rst + root->semiToneCount() + 2)
+        {
+            // Double sharped note
+            fftmp = ntmp->fn->flat;    
+            tmp = (Note*) fftmp;
+        }   
+        else if(ntmp->semiToneCount()== rst + root->semiToneCount() - 2)
+        {
+            // Double flatted note
+            sstmp = ntmp->sn->sharp;    
+            tmp = (Note*) sstmp;
+        }  
+        else
+        {
+            std::cout<< ntmp->semiToneCount() +11 << " and "<<rst<< "+"<<root->semiToneCount()<<std::endl;
+            throw E_CHORD_UNKNOWN;
+        }
+        std::cout << tmp->display() << std::endl;
+        notes.push_back(tmp);
     }
-    //TODO: handles semitones before pushing back
-    notes.push_back(tmp);
-    std::cout << tmp->display() << std::endl;
-    tmp = root->getNatural();
-    type = intervals.at(1).getType();
-    for (int i = 0; i < type - 1; i++)
-    {
-        tmp = tmp->following;
-    }
-    //TODO: handles semitones before pushing back
-    notes.push_back(tmp);
-    std::cout << tmp->display() << std::endl;
+
+//    tmp = root->getNatural();
+//    type = intervals.at(1).getType();
+//    for (int i = 0; i < type - 1; i++)
+//    {
+//        ntmp = ntmp->following;
+//    }
+//    //TODO: handles semitones before pushing back
+//    std::cout << "fifth = " << tmp->display() << std::endl;
+//    notes.push_back(ntmp);
 }
 
 Chord::Chord(Note *n, std::string str) : root(n), name(str)
@@ -64,6 +115,13 @@ Chord::Chord(Note *n, std::string str) : root(n), name(str)
     {
         intervals.push_back(Interval("major second"));
         intervals.push_back(Interval("perfect fifth"));
+        setNotes();
+    }
+    else if (!name.compare("m7"))
+    {
+        intervals.push_back(Interval("minor third"));
+        intervals.push_back(Interval("perfect fifth"));
+        intervals.push_back(Interval("minor seventh"));
         setNotes();
     }
     else if (!name.compare("7"))
