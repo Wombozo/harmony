c653f0524088452fd4f076f1399ad5a92ca8390f
guillaume <guillaume.lavigne@free.fr>
2019-05-17 00:11:20 +0200

    Removing useless interval constructor

--- Interval.cpp
+++ Interval.cpp
@@ -1,47 +1,5 @@
 #include "Interval.h"
 
-Interval::Interval(int t, int s)
-{
-    type = t;
-    semitones = s;
-    std::string stype, sst;
-    switch (type)
-    {
-    case 2:
-        stype = "second";
-        sst = (semitones == 1) ? "minor" : (semitones == 2) ? "major" : (semitones == 3) ? "augmented" : throw E_INTERVAL;
-        break;
-    case 3:
-        stype = "third";
-        sst = (semitones == 2) ? "diminished" : (semitones == 3) ? "minor" : (semitones == 4) ? "major" : (semitones == 5) ? "augmented" : throw E_INTERVAL;
-        break;
-    case 4:
-        stype = "fourth";
-        sst = (semitones == 4) ? "diminished" : (semitones == 5) ? "perfect" : (semitones == 6) ? "augmented" : throw E_INTERVAL;
-        break;
-    case 5:
-        stype = "fifth";
-        sst = (semitones == 6) ? "diminished" : (semitones == 7) ? "perfect" : (semitones == 8) ? "augmented" : throw E_INTERVAL;
-        break;
-    case 6:
-        stype = "sixth";
-        sst = (semitones == 7) ? "diminished" : (semitones == 8) ? "minor" : (semitones == 9) ? "major" : (semitones == 10) ? "augmented" : throw E_INTERVAL;
-        break;
-    case 7:
-        stype = "seventh";
-        sst = (semitones == 9) ? "diminished" : (semitones == 10) ? "minor" : (semitones == 11) ? "major" : (semitones == 11) ? "augmented" : throw E_INTERVAL;
-        break;
-    case 8:
-        stype = "octave";
-        sst = (semitones == 12) ? "perfect" : throw E_INTERVAL;
-        break;
-    default:
-        throw 0;
-        break;
-    }
-    name = sst + " " + stype;
-}
-
 Interval::Interval(std::string str)
 {
     if (!str.compare("minor second"))
